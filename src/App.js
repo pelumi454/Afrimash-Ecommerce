aa401488a18882988e6176e5a35b0c7d9c5cbc0e
pelumi454 <amoochristianah454@gmail.com>
2022-01-28 13:46:17 -0800

    add flex container to search input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Searchbar from './components/Searchbar/Searchbar';
 
 function App() {
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -3,47 +3,43 @@ import styled from "styled-components";
 
 function Searchbar() {
   return (
-    <div>
-      <Container>
-        <h2>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Purus
-          consectetur id facilisi sodales sollicitudin malesuada proin. Suscipit
-          risus ut porttitor cursus consequat iaculis habitant enim. Lorem ipsum
-          dolor sit amet, consectetur adipiscing elit. Purus consectetur id
-          facilisi sodales sollicitudin malesuada proin. Suscipit risus ut
-          porttitor cursus consequat iaculis habitant enim. Lorem ipsum dolor
-          sit amet, consectetur adipiscing elit. Purus consectetur id facilisi
-          sodales sollicitudin malesuada proin. Suscipit risus ut porttitor
-          cursus consequat iaculis habitant enim.Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit. Purus consectetur id facilisi sodales
-          sollicitudin malesuada proin. Suscipit risus ut porttitor cursus
-          consequat iaculis habitant enim.Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit. Purus consectetur id facilisi sodales
-          sollicitudin malesuada proin. Suscipit risus ut porttitor cursus
-          consequat iaculis habitant enim.Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit. Purus consectetur id facilisi sodales
-          sollicitudin malesuada proin. Suscipit risus ut porttitor cursus
-          consequat iaculis habitant enim.
-        </h2>
-      </Container>
+    <div className="container">
+      <Wrapper>
+        <>
+          <div>
+            <i class="bi bi-search"></i>
+            <input type="text" />
+          </div>
+          <SearchBtn>Search</SearchBtn>
+        </div>
+      </Wrapper>
     </div>
   );
 }
 
 export default Searchbar;
 
-const Container = styled.div`
-  min-height: 60px;
-  position: fixed;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 20px;
-  top: 0;
-  left: 0;
-  right: 0;
-  z-index: 1;
-  background-color: red;
+const Wrapper = styled.div`
+  width: 80%;
+  width: 796px;
+  height: 64px;
+  border: 0.6px solid #b7abab;
+  box-sizing: border-box;
+  border-radius: 30px;
 `;
 
-const Menu = styled.div``;
+const SearchBtn = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`;
