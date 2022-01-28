42c494968c5836e967b491e38a7b461387497df9
pelumi454 <amoochristianah454@gmail.com>
2022-01-28 14:25:39 -0800

    add flex container to search input

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -5,13 +5,13 @@ function Searchbar() {
   return (
     <div className="container">
       <Wrapper>
-        <>
-          <div>
+        <SearchContainer>
+          <SearchIconWrap>
             <i class="bi bi-search"></i>
-            <input type="text" />
-          </div>
+            <input type="text" placeholder="search anything" className="search-input"/>
+          </SearchIconWrap>
           <SearchBtn>Search</SearchBtn>
-        </div>
+        </SearchContainer>
       </Wrapper>
     </div>
   );
@@ -20,26 +20,35 @@ function Searchbar() {
 export default Searchbar;
 
 const Wrapper = styled.div`
-  width: 80%;
-  width: 796px;
+  width: 71%;
   height: 64px;
   border: 0.6px solid #b7abab;
   box-sizing: border-box;
   border-radius: 30px;
 `;
 
+const SearchContainer = styled.div`
+display: flex;
+justify-content: space-between;
+`;
+
+const SearchIconWrap = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+`
+
+
 const SearchBtn = styled.div`
   background-color: rgba(23, 26, 32, 0.8);
   height: 40px;
   width: 256px;
   color: white;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   border-radius: 100px;
   opacity: 0.85;
   text-transform: uppercase;
   font-size: 12px;
   cursor: pointer;
+  text-align: center;
   margin: 8px;
 `;
